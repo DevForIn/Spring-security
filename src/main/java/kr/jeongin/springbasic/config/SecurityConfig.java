b3cb1b3c8d1fa445d755c43c7190c1673015b7d1
JeongJeongIn <devforin@gmail.com>
2024-09-25 13:35:46 +0900

    description add

--- src/main/java/kr/jeongin/springbasic/config/SecurityConfig.java
+++ src/main/java/kr/jeongin/springbasic/config/SecurityConfig.java
@@ -17,17 +17,22 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 public class SecurityConfig {
 
+    
+    // JWT 필터 의존성 주입
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
+    // SecurityConfig 생성자 - JWT 필터 주입
     public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter){
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
     }
 
+    // AuthenticationManager Bean 생성
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
+    // 비밀번호 암호화에 사용할 BCrypt 인코더 생성
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
@@ -36,14 +41,15 @@ public class SecurityConfig {
     @Bean
     protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(csrf -> csrf.disable())   // CSRF(Cross-Site Request Forgery) 보호 비활성화
                         .authorizeHttpRequests(auth ->
                                 auth
-                                        .requestMatchers("/login","/auth/login").permitAll()
-                                        .anyRequest().authenticated()
+                                        .requestMatchers("/login","/auth/login").permitAll()     // "/login", "/auth/login" 경로는 인증 없이 접근 허용
+                                        .anyRequest().authenticated()       // 그 외 모든 요청은 인증 필요
                         )
-                                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // 세션 관리 비활성화(JWT 사용을 위한 무상태 설정)
 
+        // JWT 필터를 UsernamePasswordAuthenticationFilter 앞에 추가
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
